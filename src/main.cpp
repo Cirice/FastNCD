928e08454729330295650274a1daae2ff1b4e544
Hassan Abedi <hassan.abedi.t@gmail.com>
2019-08-21 13:54:49 +0200

    fixed a few noobish mistakes

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,12 @@
 using namespace std;
 
 int main() {
-	
-	const char x[] = "I am a sentence.";
-	const char y[] = "I am also a sentence.";
-	
+
+	const char *x = "I am a sentence.";
+	const char *y = "I am also a sentence.";
+
 	NCD ncd = NCD();
 	cout << ncd.calculate_ncd(x, y, zlib) << endl;
-	
+
 	return 0;
 }
--- src/ncd.cpp
+++ src/ncd.cpp
@@ -1,7 +1,7 @@
 #include "cmp_algorithms.hpp"
 #include "ncd.hpp"
 
-#include <algorithm> 
+#include <algorithm>
 
 
 int NCD::compress(const char* x, cmp_type t) {
@@ -12,7 +12,7 @@ const char* NCD::concat_xy(const char* x, const char* y){
 	const char r[] = "lolz";
 	return r;
 	};
-	
+
 float NCD::calculate_ncd(const char* x, const char* y, cmp_type t) {
 	return (float)(compress(concat_xy(x, y)) - std::min(compress(x, y)) / std::max(compress(x), compress(y)))
 	};
